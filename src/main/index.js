8f639e9e2fcdce87f4ccc8881add4f0a2353a18d
kronosGR <geo.elgeo@gmail.com>
2024-04-06 13:49:18 +0200

    added start stop reset function

--- src/main/index.js
+++ src/main/index.js
@@ -6,8 +6,8 @@ import icon from '../../resources/icon.png?asset'
 function createWindow() {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 900,
-    height: 670,
+    width: 500,
+    height: 500,
     show: false,
     autoHideMenuBar: true,
     ...(process.platform === 'linux' ? { icon } : {}),
@@ -50,7 +50,7 @@ app.whenReady().then(() => {
   })
 
   // IPC test
-  ipcMain.on('ping', () => console.log('pong'))
+  //ipcMain.on('ping', () => console.log('pong'))
 
   createWindow()
 
--- src/renderer/src/App.jsx
+++ src/renderer/src/App.jsx
@@ -1,35 +1,61 @@
-import Versions from './components/Versions'
-import electronLogo from './assets/electron.svg'
+import { useState, useEffect } from 'react'
 
+import './App.css'
 function App() {
-  const ipcHandle = () => window.electron.ipcRenderer.send('ping')
+  //const ipcHandle = () => window.electron.ipcRenderer.send('ping')
+
+  const [timer, setTimer] = useState(0)
+  const [isRunning, setIsRunning] = useState(false)
+
+  useEffect(() => {
+    let intervalId
+    if (isRunning) {
+      intervalId = setInterval(() => {
+        setTimer(timer + 1)
+      }, 10)
+    }
+    return () => clearInterval(intervalId)
+  }, [isRunning, timer])
+
+  const startStop = () => {
+    setIsRunning(!isRunning)
+  }
+
+  const reset = () => {
+    setTimer(0)
+  }
+
+  const TimerToString = () => {
+    const hours = Math.floor(timer / 360000)
+    const mins = Math.floor((timer % 360000) / 6000)
+    const secs = Math.floor((timer % 6000) / 100)
+    const ms = timer % 100
+
+    return (
+      <div>
+        {hours}:{mins.toString().padStart(2, '0')}:{secs.toString().padStart(2, '0')}:
+        {ms.toString().padStart(2, '0')}
+      </div>
+    )
+  }
 
   return (
-    <>
-      <img alt="logo" className="logo" src={electronLogo} />
-      <div className="creator">Powered by electron-vite</div>
-      <div className="text">
-        Build an Electron app with <span className="react">React</span>
-      </div>
-      <p className="tip">
-        Please try pressing <code>F12</code> to open the devTool
-      </p>
-      <div className="actions">
-        <div className="action">
-          <a href="https://electron-vite.org/" target="_blank" rel="noreferrer">
-            Documentation
-          </a>
+    <div className="content">
+      <h1 id="title">Chronos</h1>
+      <div className="content-timer">
+        <button type="button" id="start-stop" onClick={startStop}>
+          {isRunning ? 'Stop' : 'Start'}
+        </button>
+        <div id="timer">
+          <TimerToString />
         </div>
-        <div className="action">
-          <a target="_blank" rel="noreferrer" onClick={ipcHandle}>
-            Send IPC
-          </a>
-        </div>
+        <button type="button" id="reset-lap" onClick={reset}>
+          Reset
+        </button>
       </div>
-      <Versions></Versions>
-    </>
+      <div className="laps"></div>
+    </div>
   )
 }
 
 export default App
-
