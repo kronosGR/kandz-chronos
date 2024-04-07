543a8519d08aa996cfb3db3f9bf749af72b1f1f6
kronosGR <geo.elgeo@gmail.com>
2024-04-07 14:09:23 +0200

    added navigation and started timer

--- src/renderer/src/App.jsx
+++ src/renderer/src/App.jsx
@@ -1,15 +1,24 @@
 import './App.css'
 import Navbar from './components/Navbar'
 import StopWatch from './pages/StopWatch'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Timer from './pages/Timer'
 function App() {
   //const ipcHandle = () => window.electron.ipcRenderer.send('ping')
 
   return (
-    <div className="content">
-      <h1 id="title">Chronos</h1>
-      <StopWatch />
-      <Navbar />
-    </div>
+    <BrowserRouter>
+      <div className="content">
+        <h1 id="title">Chronos</h1>
+        <div className="pages">
+          <Routes>
+            <Route path="/" exact element={<StopWatch />} />
+            <Route path="/timer" element={<Timer />} />
+          </Routes>
+        </div>
+        <Navbar />
+      </div>
+    </BrowserRouter>
   )
 }
 
--- src/renderer/src/components/Navbar.jsx
+++ src/renderer/src/components/Navbar.jsx
@@ -1,5 +1,13 @@
+import { Link } from 'react-router-dom'
+import './Navbar.css'
+
 const Navbar = () => {
-  return <>Navbar</>
+  return (
+    <div className="navbar">
+      <Link to="/">Stop Watch</Link>
+      <Link to="/timer">Timer</Link>
+    </div>
+  )
 }
 
 export default Navbar
